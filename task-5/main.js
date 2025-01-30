4fc3d6a205f762d2e8a825a6b196ae6543b9929a
rafiq ramazanov <rafiq12.dev@gmail.com>
2025-01-30 12:25:20 +0400

    homework done

---
+++ task-5/main.js
@@ -0,0 +1,26 @@
+//                1
+// function showMessage(array,index){
+//     return array[index];
+//  }
+//  console.log(showMessage([1,2,3,4,5],3));
+//                2
+// function getSum(arr){
+//     return arr.slice(3);
+// }
+// console.log(getSum([1,2,3,4,5,6]))
+
+// 3 yaza bilmedim
+//               4 
+// function getSum(number){
+//     let numbers=number.filter((number)=>number<0);
+//     return numbers
+// }
+// console.log(getSum([1,-2,2,-4]));
+//                5
+// function get(arr=[]){
+//     let sum=0;
+//     arr.map((i)=> sum+=i)
+//     return sum/arr.length    
+// }
+// let result=get([10, 100,40])
+// console.log(result)
